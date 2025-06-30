ce607b1cb49960173e7b91fbf36f3f7c552d6519
Prashant kumar <114512068+Prashant-kr01@users.noreply.github.com>
2025-06-30 03:50:28 -0700

    Create index.js

---
+++ Server/src/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = require("./router");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+// Apply JSON parsing middleware
+app.use(express.json());
+
+// Apply router
+app.use("/", router);
+
+// Serving app on defined PORT
+app.listen(PORT, () => {
+  console.log(`Express is running on port ${PORT}`);
+});
